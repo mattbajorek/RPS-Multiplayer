debaef7c08fd93ace029cb420b84320d8f4e4bfa
Matthew Bajorek <bajorek.matthew@gmail.com>
2016-06-10 21:59:45 -0400

    Working playing adding with firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,28 +3,74 @@ $(document).on('ready', function() {
 	var database = new Firebase("https://game-rock-paper-scissors.firebaseio.com/");
 	// Link to Firebase for viewer tracking
 	var presenceRef = new Firebase('https://game-rock-paper-scissors.firebaseio.com/.info/connected');
-	var userRef = new Firebase('https://game-rock-paper-scissors.firebaseio.com/users');
-	var userData = userRef.push();
-	var userID = userData.key();
+	var players = new Firebase('https://game-rock-paper-scissors.firebaseio.com/players');
+	var player = 1;
+	//var userID = userRef.key();
+	var userRef;
+
+	$('#addName').on('click',function() {
+		database.once('value', function(snapshot) {
+		  if (snapshot.val() == null) {
+		  	addPlayer(player);
+		  } else {
+		  	player++;
+		  	addPlayer(player);
+		  }
+		});
+		return false;
+	});
+
+	function addPlayer(count) {
+		userRef = players.child(count);
+		userRef.onDisconnect().remove();
+		userRef.set({
+			'name': 'Matt',
+			'wins': 0,
+			'losses': 0
+		});
+	}
+
+	/*players.on('value', function(snapshot) {
+	  console.log(snapshot.val());
+	});	
+
+	presenceRef.on('value', function(snapshot) {
+	  if (snapshot.val()) {
+	  	var userRef = players.child(count);
+		  userRef.onDisconnect().remove();
+		  userRef.set({
+				'name': 'Matt',
+				'wins': 0,
+				'losses': 0
+				//'userID': userID
+			});
+	  }
+	});
 
 	// Game Object
 	var game = {
 		presence: function() {
 			presenceRef.on('value', function(snapshot) {
+				console.log(snapshot.val());
 			  if (snapshot.val()) {
-			    userData.onDisconnect().remove();
-			    userData.set(true);
+			    userRef.onDisconnect().remove();
+			    userRef.set(true);
 			  }
 			});
 		},
 		setPlayer: function() {
-			userRef.on('value', function(snapshot) {
+			listRef.on('value', function(snapshot) {
 				//if (userID == snapshot.val().user)
-			  console.log(snapshot.val());
+			  console.log(snapshot.numChildren());
+			  var num = snapshot.numChildren();
+			  var obj = {};
+			  obj[num] = userID;
+			  player.onDisconnect().remove();
+			  player.update(obj);
 			});
 		}
 	}
 	// Initialize presence
 	game.presence();
-	game.setPlayer();
+	game.setPlayer();*/
 });
